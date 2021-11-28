9dd7175c2332eaf8f55ce221ae854e92e90a46d6
Merveille Jora <88484917+MerveilleJora@users.noreply.github.com>
2021-11-28 21:55:10 +0100

    Add files via upload

---
+++ CLserviceClient.h
@@ -0,0 +1,20 @@
+#include "CLPersonne.h"
+#include "CL_CAD.h"
+
+namespace NS_Comp_Svc
+{
+	ref class CLserviceClient//service gestion de clients 
+	{
+	private:
+		//Objet de composant 
+		NS_Comp_Data::CL_CAD^ oCad;
+		NS_Comp_Mappage::CLPersonne^ oTBPersonne;
+	public:
+		CLserviceClient(void);//constructeur 
+		//les autres méthodes
+		System::Data::DataSet^ afficherClient(System::String^);
+		void creerClient(System::String^, System::String^);
+		void supprimerClient(System::String^, System::String^);
+		void modifierClient(System::String^, System::String^);
+	};
+}
