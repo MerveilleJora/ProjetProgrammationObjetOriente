25f4448560475c798452e740e5740324ef9cc542
VictorCESI <95218831+VictorCESI@users.noreply.github.com>
2021-12-01 17:40:32 +0100

    Update CLservicecommande.h

--- CLservicecommande.h
+++ CLservicecommande.h
@@ -1,44 +1,20 @@
-#include "CLserviceCommande.h"
+#include "CLCommande.h"
+#include "CL_CAD.h"
 
-
-NS_Comp_Svc::CLserviceCommande::CLserviceCommande(void)
+namespace NS_Comp_Svc
 {
-	this->oCad = gcnew NS_Comp_Data::CL_CAD();
-	this->oTBCommande = gcnew NS_Comp_Mappage::CLCommande();
-}
-System::Data::DataSet^ NS_Comp_Svc::CLserviceCommande::afficherCommande(System::String^ dataTableName)
-{
-	System::String^ sSql;
-
-	sSql = this->oTBCommande->Select();
-	return this->oCad->getRows(sSql, dataTableName);
-}
-void NS_Comp_Svc::CLserviceCommande::creerCommande(int Id, System::String^ nom, System::String^ prenom, System::String^ date)
-{
-	System::String^ sSql;
-	this->oTBCommande->setReference(Id);
-	this->oTBCommande->setNom(nom);
-	this->oTBCommande->setPrenom(prenom);
-	this->oTBCommande->setDate(date);
-	sSql = this->oTBCommande->Insert();
-
-	this->oCad->actionRows(sSql);
-}
-
-void NS_Comp_Svc::CLserviceCommande::supprimerCommande(int Id)
-{
-	System::String^ sSql;
-	this->oTBCommande->setReference(Id);
-	sSql = this->oTBCommande->Delete();
-
-	this->oCad->actionRows(sSql);
-}
-
-void NS_Comp_Svc::CLserviceCommande::modifierCommande(int Id)
-{
-	System::String^ sSql;
-	this->oTBCommande->setReference(Id);
-	sSql = this->oTBCommande->Update();
-
-	this->oCad->actionRows(sSql);
+	ref class CLserviceCommande//service gestion de personnels 
+	{
+	private:
+		//Objet de composant 
+		NS_Comp_Data::CL_CAD^ oCad;
+		NS_Comp_Mappage::CLCommande^ oTBCommande;
+	public:
+		CLserviceCommande(void);//constructeur 
+		//les autres méthodes
+		System::Data::DataSet^ afficherCommande(System::String^);
+		void creerCommande(System::String^, System::String^, System::String^, System::String^,System::String^, System::String^, System::String^, System::String^, int, int, double, double, double, System::String^, System::String^, int, System::String^, System::String^, System::String^, System::String^, System::String^);
+		void supprimerCommande(System::String^);
+		void modifierCommande(System::String^);
+	};
 }
