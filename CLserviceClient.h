3dc59191d58a1ca3f2061155b950528fb5536186
VictorCESI <95218831+VictorCESI@users.noreply.github.com>
2021-12-02 14:32:02 +0100

    Update CLserviceClient.h

--- CLserviceClient.h
+++ CLserviceClient.h
@@ -6,14 +6,14 @@ namespace NS_Comp_Svc
 	ref class CLserviceClient//service gestion de clients 
 	{
 	private:
-		//Objet de composant 
+		//Objet de composant
 		NS_Comp_Data::CL_CAD^ oCad;
 		NS_Comp_Mappage::CLPersonne^ oTBPersonne;
 	public:
 		CLserviceClient(void);//constructeur 
 		//les autres méthodes
 		System::Data::DataSet^ afficherClient(System::String^);
-		void creerClient(int, System::String^, System::String^, System::String^, System::String^);
+		void creerClient(System::String^, System::String^, System::String^, int, System::String^, System::String^, int, System::String^, System::String^, System::String^);
 		void supprimerClient(int);
 		void modifierClient(int);
 	};
